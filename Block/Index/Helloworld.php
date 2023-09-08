c896c4d0acf945aeedc2ba53a9a029085744f9e9
Jonathan Urzola Maldonado <jonathan.urzola@outlook.com>
2023-09-08 08:50:20 -0500

    First commit

---
+++ Block/Index/Helloworld.php
@@ -0,0 +1,31 @@
+<?php
+namespace Curso\HelloWorld\Block\Index;
+
+/**
+ * Class Helloworld 
+ *
+ * @package Curso\HelloWorld\Block\Index;
+ */
+class Helloworld extends \Magento\Framework\View\Element\Template
+{
+    /**
+     * Constructor
+     *
+     * @param \Magento\Framework\View\Element\Template\Context  $context
+     * @param array $data
+     */
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+    }
+
+    /**
+     * @return string
+     */
+    public function getHelloWorldMessage()
+    {
+        return 'Hello World!';
+    }
+}
